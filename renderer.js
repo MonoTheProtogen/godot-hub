f59016e6a47b79648f23b3231e65558b546bcac1
Mono The Protogen <64149624+MonoTheProtogen@users.noreply.github.com>
2024-07-02 17:37:12 +0200

    update renderer.js, style.css

--- renderer.js
+++ renderer.js
@@ -18,3 +18,35 @@ document.querySelector('.close').addEventListener("click", closeApp)
 document.querySelector('.minimize').addEventListener("click", minimizeApp)
 
 document.querySelector('.credits').addEventListener("click", openCredits)
+
+// Main view portion
+
+const body = document.querySelector(".body")
+
+const projects = document.querySelector(".projects")
+const versions = document.querySelector(".versions")
+const settings = document.querySelector(".settings")
+
+var bodyTab = 0 // 0 = Projects tab, 1 = Versions tab, 2 = Settings tab
+
+projects.addEventListener("click", function() {bodyTab = 0; bodyPrint()})
+versions.addEventListener("click", function() {bodyTab = 1; bodyPrint()})
+settings.addEventListener("click", function() {bodyTab = 2; bodyPrint()})
+
+function bodyPrint() {
+    body.innerHTML = ""
+    switch(bodyTab) {
+        case 0:
+            body.innerHTML += "<h2>Projects</h2>"
+
+            break;
+        case 1:
+            body.innerHTML += "<h2>Versions</h2>"
+
+            break;
+        case 2:
+            body.innerHTML += "<h2>Settings</h2>"
+    }
+}
+
+bodyPrint()
