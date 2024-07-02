244a137f93eaf9e1e2ed21366086f3adb6a727fc
Mono The Protogen <64149624+MonoTheProtogen@users.noreply.github.com>
2024-07-02 22:44:57 +0200

    update renderer.js

--- renderer.js
+++ renderer.js
@@ -1,3 +1,5 @@
+// titlebar functions
+
 const ipc = require('electron').ipcRenderer
 
 function closeApp(e) {
@@ -54,16 +56,20 @@ function bodyPrint() {
         case 2:
             body.innerHTML += '<div class="utilitybar"> <h2 class="title">Settings</h2> <p class="setsave">Apply</p>  </div>'
 
-            body.innerHTML += '<div class="settingbody"> <p>Projects folder path: </p> <p>Godot versions folder path: </p> <p class="note">Note: Changes won\'t take effect until clicking the apply button and restarting the app.</p> </div>'
+            body.innerHTML += '<div class="settingbody"> <p>Projects folder path: <input type="text" class="projpath"> <input type="button" value="Browse" class="projbrowse"> </p> <p>Godot versions folder path: <input type="text" class="verpath"> <input type="button" value="Browse" class="verbrowse"> </p> <p class="note">Note: Changes won\'t take effect until clicking the apply button and restarting the app.</p> </div>'
             
             const projpath = document.querySelector(".projpath")
+            const projbrowse = document.querySelector(".projbrowse")
             const verpath = document.querySelector(".verpath")
+            const verbrowse = document.querySelector(".verbrowse")
             const setsave = document.querySelector(".setsave")
 
             setsave.addEventListener("click", function() {
                 console.log(projpath.value)
                 console.log(verpath.value)
             })
+
+            
 
             break;
     }
