d3860a8c4c0be29f811580e48d1f22c39002dc3d
Mono The Protogen <64149624+MonoTheProtogen@users.noreply.github.com>
2024-07-03 23:10:57 +0200

    Update forge.config.js

--- forge.config.js
+++ forge.config.js
@@ -4,7 +4,7 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 module.exports = {
   packagerConfig: {
     asar: true,
-    icon: './images/icons/hub_icon_512x512'
+    icon: './images/icons/hub_icon_512x512',
   },
   rebuildConfig: {},
   makers: [
