8c73633db0cbca6e23664863863ff8cbd2b8b6a4
Mono The Protogen <64149624+MonoTheProtogen@users.noreply.github.com>
2024-08-12 23:39:15 +0200

    update renderer.js, style.css

--- renderer.js
+++ renderer.js
@@ -59,7 +59,7 @@ function bodyPrint() {
 
             break;
         case 1:
-            body.innerHTML += '<div class="utilitybar"> <h2 class="title">Versions</h2> <p class="monop">Mono (C# Support) <input type="checkbox"></p> <p class="mirrorp">Version: <select class="mirror"><option value="stable">Stable</option><option value="rc">RC</option><option value="beta">Beta</option><option value="dev">Dev</option> </select></p> </div>'
+            body.innerHTML += '<div class="utilitybar"> <h2 class="title">Versions</h2> <p class="mirrorp">Version: <select class="mirror"><option value="stable">Stable</option><option value="mono">Stable (Mono)</option><option value="rc">RC</option><option value="beta">Beta</option><option value="dev">Dev (Alpha)</option> </select></p> </div>'
 
 
 
